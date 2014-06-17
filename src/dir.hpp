b3c3321a1c3761aa9795b06f475c924f821ffd4f
bumbar1 <nejc.drascek@gmail.com>
2014-06-17 02:13:32 +0200

    inlined dir

--- src/dir.hpp
+++ src/dir.hpp
@@ -12,33 +12,92 @@ namespace mmx {
 
 	class MMX_API dir {
 	public:
-		dir();
-		dir(const std::string& path);
+		dir()
+			: dir(".")
+		{
+		}
+
+		dir(const std::string& path)
+			: _dir(nullptr)
+			, _pdir(nullptr)
+			, _path(path)
+		{
+			open();
+		}
 
 		dir(const dir&) MMX_CLASS_DELETE;
 		dir& operator = (const dir& ) MMX_CLASS_DELETE;
+		dir(dir&&) MMX_CLASS_DELETE;
+		dir&& operator = (dir&&) MMX_CLASS_DELETE;
 
-		virtual ~dir();
+		virtual ~dir(
+			close();
+		);
 
-		void                     open(const std::string& path);
-		void                     open();
-		void                     close();
-		void                     rewind();
+		void open() {
+			if (_path.empty()) {
+				throw std::runtime_error("dir: no directory specified to open");
+			}
+
+			close();
+
+			_dir = ::opendir(_path.c_str());
+
+			if (_dir == nullptr) {
+				throw std::runtime_error("dir: failed to open '" + _path + "'");
+			}
+		}
+
+		void open(const std::string& path) {
+			_path = path;
+			open();
+		}
+
+		void close() {
+			if (_dir) {
+				::closedir(_dir);
+			}
+		}
+
+		void rewind() {
+			::rewinddir(_dir);
+		}
+		
+
+		void read(std::vector<std::string>& files) {
+			while ((_pdir = ::readdir(_dir))) {
+				files.push_back(_pdir->d_name);
+			}
+		}
+
+		std::vector<std::string> dir::read() {
+			std::vector<std::string> files;
+			read(files);
+			return files;
+		}
+
+		std::string path() const {
+			return _path;
+		}
+
+		bool is_open() const {
+			return _dir != nullptr;
+		}
+
 	#if !defined( MMX_IS_MSVC )
-		void                     seek(unsigned long location);
+		void seek(unsigned long location) {
+			seekdir(_dir, location);
+		}
+
+		ulonglong offset() const {
+			return telldir(_dir);
+		}
 	#endif
-		void                     read(std::vector<std::string>& files);
-		std::vector<std::string> read();
-		std::string              path() const;
-	#if !defined( MMX_IS_MSVC )
-		unsigned long long       offset() const;
-	#endif
-		bool                     is_open() const;
 
 	private:
-		DIR*                     _dir;
-		dirent*                  _pdir;
-		std::string              _path;
+		DIR*         _dir;
+		dirent*      _pdir;
+		std::string  _path;
 	};
 
 }           // ~namespace mmx
